6b3e82b0fea2baeea1a1df7d04047bc8ac7e89f0
Aditya <adityaa0544@gmail.com>
2024-11-18 16:16:26 +0530

    canvas can go back to dashboard

--- src/sections/logo-section.js
+++ src/sections/logo-section.js
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png';
 export const LogoSection = {
   name: 'logo',
   Tab: (props) => (
-      <div className=' md:py-4 max-md:px-4 flex'>
+      <div className=' md:py-4 max-md:px-4 flex' onClick={()=>window.location.href="/dashboard"}>
         <img src={logo} alt='logo' className='mx-auto mt-auto mb-1 md:h-[42px] max-md:h-[36px]'  />
       </div>
   ),
